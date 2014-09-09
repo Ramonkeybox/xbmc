1f5d539b5379e8fe5b4d0891665e06f9bfd3e10b
Linus Yang <laokongzi@gmail.com>
2014-09-09 13:43:02 +0800

    ios: arm64: disable video scale down for arm64
    Fix commit 0624190 issue

--- xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecVideoToolBox.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/DVDVideoCodecVideoToolBox.cpp
@@ -1544,6 +1544,8 @@ CDVDVideoCodecVideoToolBox::CreateVTSession(int width, int height, CMFormatDescr
         width = new_width;
         height = height * w_scaler;
       }
+
+      #if !defined(__LP64__)
       // scale output pictures down to 720p size for display
       if (width > width_clamp)
       {
@@ -1551,6 +1553,7 @@ CDVDVideoCodecVideoToolBox::CreateVTSession(int width, int height, CMFormatDescr
         width = width_clamp;
         height = height * w_scaler;
       }
+      #endif
     }
   #endif
   destinationPixelBufferAttributes = CFDictionaryCreateMutable(
